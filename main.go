77d5ba2b6ad9aa514f14e9357793e5e35dcc1450
bluebore <bluebore.cn@gmail.com>
2014-10-19 07:34:36 +0800

    add func1

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+var name = "bluebore"
+
+func func1() {
+	print("call func1\n");
+	print(name)
+	fmt.Println("\n")
+}
+
+const PI = 3.14
+
+type node struct{
+	left *node;
+	right *node;
+	value string;
+}
+
+type golang interface{}
+
 func main() {
 	print("Hello golang\n")
+	func1()
 }
